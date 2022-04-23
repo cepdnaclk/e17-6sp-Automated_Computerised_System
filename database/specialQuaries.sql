935a1d76b9c43349aee39202164156ee2496bb32
AdithyaGallage <e17091@eng.pdn.ac.lk>
2022-04-23 16:49:07 +0530

    privileges were defined for DM and SA

--- database/specialQuaries.sql
+++ database/specialQuaries.sql
@@ -16,7 +16,6 @@ INSERT INTO factory_manager VALUES(
 CREATE ROLE 'factory_manager', 'distribution_manager', 'sales_agent';
 -- DROP ROLE 'factory_manager', 'distribution_manager', 'sales_agent';
 
--- GRANT USAGE ON victa.* TO 'factory_manager';
 GRANT SELECT, INSERT, UPDATE, DELETE ON victa.product TO 'factory_manager';
 GRANT SELECT, INSERT, UPDATE, DELETE ON victa.factory_manager TO 'factory_manager';
 GRANT SELECT, INSERT, UPDATE, DELETE ON victa.distribution_manager TO 'factory_manager';
@@ -27,6 +26,25 @@ GRANT SELECT, DELETE ON victa.issued_product TO 'factory_manager';
 GRANT CREATE USER ON *.* TO 'factory_manager';
 GRANT ROLE_ADMIN ON *.* TO 'factory_manager';
 GRANT RELOAD ON *.* TO 'factory_manager';
+FLUSH PRIVILEGES;
+
+GRANT SELECT, INSERT, UPDATE, DELETE ON victa.sales_agent TO 'distribution_manager';
+GRANT INSERT (ReceivedQuantity, ValidRecord, CheckedDMUserName) ON victa.issued_product TO 'distribution_manager';
+GRANT UPDATE (ReceivedQuantity, ValidRecord) ON victa.issued_product TO 'distribution_manager';
+GRANT SELECT, DELETE ON victa.issued_product TO 'distribution_manager';
+GRANT SELECT, INSERT, UPDATE, DELETE ON victa.shop TO 'distribution_manager';
+GRANT SELECT, INSERT, UPDATE, DELETE ON victa.distributed_product TO 'distribution_manager';
+GRANT INSERT (DBatchNumber, DShippingDate, IssuedDMUserName, SalesAgentUSerName, DestinationShopID, DelivaryStatus, ValidRecord) ON victa.distribution_process TO 'distribution_manager';
+GRANT UPDATE (DBatchNumber, DShippingDate, IssuedDMUserName, SalesAgentUSerName, DestinationShopID, DelivaryStatus, ValidRecord) ON victa.distribution_process TO 'distribution_manager';
+GRANT SELECT, DELETE ON victa.distribution_process TO 'distribution_manager';
+GRANT CREATE USER ON *.* TO 'distribution_manager';
+GRANT ROLE_ADMIN ON *.* TO 'distribution_manager';
+GRANT RELOAD ON *.* TO 'distribution_manager';
+FLUSH PRIVILEGES;
+
+GRANT INSERT (CourieredDate, CourieredDate, DelivaryStatus, ValidRecord) ON victa.distribution_process TO 'sales_agent';
+GRANT UPDATE (CourieredDate, CourieredDate, DelivaryStatus, ValidRecord) ON victa.distribution_process TO 'sales_agent';
+GRANT SELECT, DELETE ON victa.distribution_process TO 'sales_agent';
 FLUSH PRIVILEGES;
 
 SELECT User, Host FROM mysql.user;
