5d1b05db5c47f5886ef19945a6db68cab683a59f
AdithyaGallage <e17091@eng.pdn.ac.lk>
2022-04-24 02:33:47 +0530

    routes are updated

--- web_application/victa_towers_backend/routes/facProduct.js
+++ web_application/victa_towers_backend/routes/facProduct.js
@@ -54,7 +54,48 @@ router.post("/", auth, async (req, res) => {
 });
 
 router.post("/issue", auth, async (req, res) => {
-    // to be implement
+    const body = req.body;
+    const fromJwt = req.fromUser;
+    const connection = await databaseConnection.createConnection(fromJwt.jwtUserName, fromJwt.jwtPassWord);
+    const batchNumber = body.batchNumber;
+    const issuedDate = body.issuedDate;
+    const issuedQuantity = body.issuedQuantity;
+    const issuedProductName = body.productName;
+    const getOccurance = `SELECT * FROM issued_product WHERE batchNumber='${batchNumber}'`;
+    const insertIssuedProduct = `INSERT INTO issued_product(BatchNumber, IssuedDate, IssuedQuantity, CurrentQuantity, IssuedProductName) VALUES('${batchNumber}', '${issuedDate}', ${issuedQuantity}, ${issuedQuantity}, '${issuedProductName}');`;
+    const updateFactoryProduct = `UPDATE factory_product SET currentQuantity = currentQuantity-${issuedQuantity} WHERE batchNumber='${batchNumber}';`;
+    const token = jwt.sign(
+        {
+            jwtUserName: fromJwt.jwtUserName,
+            jwtPassWord: fromJwt.jwtPassWord,
+            jwtRole: fromJwt.jwtRole
+        },
+        "victa_jwtPrivateKey"
+    );
+    try {
+        const [res1] = await connection.promise().execute(getOccurance);
+        console.log(res1);
+        if(!res1.length){
+            const [res2] = await connection.promise().execute(insertIssuedProduct); 
+            const [res3] = await connection.promise().execute(updateFactoryProduct);
+            res.header("x-auth-token", token).status(200).send('Successfully issued');
+        }
+        else{
+            console.log(res1[0]);
+            console.log(res1[0].BatchNumber);
+            const updateIssuedProduct = `UPDATE issued_product SET issuedQuantity=${issuedQuantity}, receivingstatus=null WHERE BatchNumber='${batchNumber}';`;
+            /*
+            if(res1[0].receivingstatus == 'success'){
+
+            }
+            */
+            res.header("x-auth-token", token).status(400).send(res1);
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(400).send("Database failure"); 
+    }
+
 });
 
 module.exports = router;
--- web_application/victa_towers_backend/routes/product.js
+++ web_application/victa_towers_backend/routes/product.js
@@ -27,4 +27,28 @@ router.get("/", auth, async (req, res) => {
     }
 });
 
+router.post("/", auth, async (req, res) => {
+    const fromJwt = req.fromUser;
+    const body = req.body;
+    const connection = await databaseConnection.createConnection(fromJwt.jwtUserName, fromJwt.jwtPassWord);
+    const productName = body.productName;
+    const unitPrice = body.unitPrice;
+    const enterProduct = `INSERT INTO product VALUES('${productName}', ${unitPrice});`;
+    try {
+        const [response] = await connection.promise().execute(enterProduct);
+        const token = jwt.sign(
+            {
+                jwtUserName: fromJwt.jwtUserName,
+                jwtPassWord: fromJwt.jwtPassWord,
+                jwtRole: fromJwt.jwtRole
+            },
+            "victa_jwtPrivateKey"
+        );
+        res.header("x-auth-token", token).status(200).send(response);
+    } catch (error) {
+        console.log(error);
+        res.status(400).send("Database failure");
+    }
+});
+
 module.exports = router;
